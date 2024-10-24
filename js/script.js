709b177c294462bef5d608584e4a0815a5c24549
ClaRa-git <rameau.claire.cr@gmail.com>
2024-10-24 21:37:16 +0200

    premier test

--- js/script.js
+++ js/script.js
@@ -24,18 +24,78 @@ function fillLocalStorage() {
     localStorage.setItem('listeProduits', JSON.stringify(products));
 }
 
-fillLocalStorage();
+//fillLocalStorage();
+
+// Affichage du tableau de produits
+function displayProducts() {
+    let listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
+
+    if (listeProduits.length > 0) {
+        const bodyTableau = document.getElementById("products");
+        bodyTableau.innerHTML = '';
+
+        listeProduits.forEach(element => {
+
+            // Création de la ligne du tableau avec les données du produit
+            const tableRow = document.createElement('tr');
+
+            const tdName = document.createElement('td'); // Création de la cellule pour le nom du produit
+            tdName.classList.add('w-100');
+            tdName.textContent = element.name;
+
+            const tdPrice = document.createElement('td'); // Création de la cellule pour le prix du produit
+            tdPrice.textContent = element.price;
+
+            const tdStock = document.createElement('td'); // Création de la cellule pour le stock du produit
+            tdStock.classList.add('stock');
+            tdStock.textContent = element.stock;
+
+            const tdButtonAddMinus = document.createElement('td'); // Création de la cellule pour les boutons d'ajout et de suppression
+            tdButtonAddMinus.classList.add('text-nowrap');
+
+            const buttonMinus = document.createElement('button'); // Création du bouton de soustraction
+            buttonMinus.classList.add('btn', 'btn-primary', 'btn-sm', 'stock-add');
+            buttonMinus.innerHTML = '&minus;';
+            buttonMinus.addEventListener('click', function () {
+                console.log('retrait');
+            });
+
+            const buttonAdd = document.createElement('button'); // Création du bouton d'ajout
+            buttonAdd.classList.add('btn', 'btn-outline-primary', 'btn-sm', 'stock-del');
+            buttonAdd.innerHTML = '&plus;';
+            buttonAdd.addEventListener('click', function () {
+                console.log('ajout');
+            });
+
+            tdButtonAddMinus.append(buttonMinus, buttonAdd); // Ajout des boutons dans la cellule
+
+            const tdDelete = document.createElement('td'); // Création de la cellule pour le bouton de suppression
+            const buttonDelete = document.createElement('button');
+            buttonDelete.classList.add('btn', 'btn-danger', 'btn-sm', 'product-del');
+            buttonDelete.innerHTML = '&Cross;';
+            buttonDelete.addEventListener('click', function () {
+                if (window.confirm('Voulez-vous vraiment supprimer ce produit ?')) {
+                    element.remove();
+                }
+            });
+
+            tdDelete.append(buttonDelete);
+
+            tableRow.append(tdName, tdPrice, tdStock, tdButtonAddMinus, tdDelete);
+            bodyTableau.append(tableRow);
+        });
+    }
+}
 
 // DOMContentLoaded
 document.addEventListener('DOMContentLoaded', function () {
     console.log('%c🚀', 'font-size:180px');
     // Votre code ici !
 
-    const divAlert = document.getElementById('alert');
-
     // Récupération de la liste des produits dans le localStorage sinon un tableau vide
     let listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
 
+    const divAlert = document.getElementById('alert');
     // Gère l'affichage du message d'alerte si le stock est vide ou non
     if (listeProduits.length <= 0) {
         divAlert.style.display = 'block';
@@ -43,5 +103,30 @@ document.addEventListener('DOMContentLoaded', function () {
         divAlert.style.display = 'none';
     }
 
+    const formulaire = document.getElementById("ajoutForm");
+    const inputName = document.getElementById("add-name");
+    const inputPrice = document.getElementById("add-price");
+    const inputStock = document.getElementById("add-stock");
 
+    formulaire.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if (!inputName.value || !inputPrice.value || !inputStock.value) {
+            alert("Veuillez remplir tous les champs");
+            return;
+        }
+
+        const produit = {
+            name: inputName.value,
+            price: parseFloat(inputPrice.value),
+            stock: parseInt(inputStock.value)
+        };
+
+        listeProduits.push(produit);
+        localStorage.setItem("listeProduits", JSON.stringify(listeProduits));
+        displayProducts();
+    });
+
+    // Affichage des produits si le stock n'est pas vide
+    displayProducts();
 });
